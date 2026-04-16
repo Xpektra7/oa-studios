261813868cfe5730e1b14d8c78e8d2dc80e56168
Xpektra7 <xpektra7@gmail.com>
2026-04-16 03:02:58 +0100

    fix: update About section styling and add drop-shadow effect

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,6 +1,8 @@
+import './about.css'
+
 export default function About() {
   return (
-    <section className="relative h-[200dvh] z-50 w-screen bg-bg rounded-t-4xl p-8">
+    <section id="about" className="relative h-[200dvh] z-50 w-screen bg-bg rounded-t-4xl p-8 shadow-accent-border shadow-5xl ">
       <h1 className="text-5xl font-italic font-bold">About</h1>
     </section>
   )
