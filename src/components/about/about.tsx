618283adb21c76329a5f0e872a210a6be00f4782
Xpektra7 <xpektra7@gmail.com>
2026-04-16 02:51:40 +0100

    fix: enhance About section header styling with italic and bold fonts

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <section className="relative h-[200dvh] z-50 w-screen bg-bg rounded-t-4xl p-8">
-      <h1 className="text-3xl">About</h1>
+      <h1 className="text-5xl font-italic font-bold">About</h1>
     </section>
   )
 }
